3b1f3354d5428cb13384cd8d19b40734ef1b09be
Jacques van Helden <jacques.van-helden@univ-amu.fr>
2014-09-08 18:12:27 +0200

    preparing demo case

---
+++ jaspar_stat5ab_matrix.tf
@@ -0,0 +1,38 @@
+AC  MA0519.1
+XX
+ID  Stat5a::Stat5b
+XX
+DE  MA0519.1 Stat5a::Stat5b; from JASPAR
+PO       A     C     G     T
+1     6172  2326  4425  3584
+2     2073  3596  2901  7937
+3        0     0     0 16507
+4        0  1600     0 14907
+5        0 16341     0   166
+6      263 12795     0  3449
+7     7044  6137     0  3326
+8    11543     0  4140   824
+9        0     0 16507     0
+10   14888    93     0  1526
+11   14886     0  1621     0
+XX
+CC  program: jaspar
+CC  matrix.nb: 208
+CC  min.prior: 0.25
+CC  alphabet.size: 4
+CC  max.bits: 2
+CC  total.information: 9.23803090923878
+CC  information.per.column: 0.83982099174898
+CC  max.possible.info.per.col: 1.38629436111989
+CC  consensus.strict: atTTCCaAGAA
+CC  consensus.strict.rc: TTCTTGGAAAT
+CC  consensus.IUPAC: rtTTCCmRGAA
+CC  consensus.IUPAC.rc: TTCYKGGAAAY
+CC  consensus.regexp: [ag]tTTCC[ac][AG]GAA
+CC  consensus.regexp.rc: TTC[CT][GT]GGAAA[CT]
+CC  residues.content.crude.freq: a:0.3132|c:0.2362|g:0.1630|t:0.2876
+CC  G+C.content.crude.freq: 0.399180512950429
+CC  residues.content.corrected.freq: a:0.3132|c:0.2362|g:0.1630|t:0.2876
+CC  G+C.content.corrected.freq: 0.399186620261251
+XX
+//
