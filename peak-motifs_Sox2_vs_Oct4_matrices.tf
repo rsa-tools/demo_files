226a182cad7abf173a1c85269e5dfbd86d9f002c
root <jcastro@lcg.unam.mx>
2015-04-22 08:52:09 +0200

    Set of motifs used in matrix-clustering demo

---
+++ peak-motifs_Sox2_vs_Oct4_matrices.tf
@@ -0,0 +1,417 @@
+AC  oligos_6nt_vs_ctrl_m1
+XX
+ID  oligos_6nt_vs_ctrl_m1
+XX
+DE  rmarrACAAwrRrra
+PO       A     C     G     T
+1     1503  1015  1395  1026
+2     1450  1460  1155   874
+3     3115     0  1015   809
+4     1652   428  2135   724
+5     2826   523  1292   298
+6     4939     0     0     0
+7        0  4584   355     0
+8     4939     0     0     0
+9     4939     0     0     0
+10    2625     0     0  2314
+11    2080     0  2859     0
+12    1271     0  3668     0
+13    2657     0  2282     0
+14    1608  1184  1323   824
+15    1563  1127  1102  1147
+XX
+BA  4939 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: oligos_6nt_vs_ctrl_m1
+CC  AC: oligos_6nt_vs_ctrl_m1
+CC  id: oligos_6nt_vs_ctrl_m1
+CC  version: 
+CC  name: oligos_6nt_vs_ctrl_m1
+CC  description: rmarrACAAwrRrra
+CC  statistical_basis: 4939 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  sites: 4939
+CC  bg_markov_order: 1
+CC  consensus.strict: gcagaACAAagGaaa
+CC  consensus.strict.rc: TTTCCTTTGTTCTGC
+CC  consensus.IUPAC: rmarrACAAwrGrra
+CC  consensus.IUPAC.rc: TYYCYWTTGTYYTKY
+CC  consensus.regexp: [ag][ac]a[ag][ag]ACAA[at][ag]G[ag][ag]a
+CC  consensus.regexp.rc: T[CT][CT]C[CT][AT]TTGT[CT][CT]T[GT][CT]
+XX
+//
+AC  oligos_6nt_vs_ctrl_m2
+XX
+ID  oligos_6nt_vs_ctrl_m2
+XX
+DE  rmrrrASAAwrsamTGsa
+PO       A     C     G     T
+1      190   110   179   119
+2      175   155   137   131
+3      304     1   158   135
+4      219    59   201   119
+5      286    86   156    70
+6      598     0     0     0
+7        0   424   173     1
+8      488   109     1     0
+9      469   129     0     0
+10     379     1     0   218
+11     281     0   258    59
+12       0   281   317     0
+13     383   127    88     0
+14     374   222     1     1
+15       0     1     0   597
+16       2     2   594     0
+17     145   155   157   141
+18     199   131   142   126
+XX
+BA  598 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 2
+CC  accession: oligos_6nt_vs_ctrl_m2
+CC  AC: oligos_6nt_vs_ctrl_m2
+CC  id: oligos_6nt_vs_ctrl_m2
+CC  version: 
+CC  name: oligos_6nt_vs_ctrl_m2
+CC  description: rmrrrASAAwrsamTGsa
+CC  statistical_basis: 598 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 2
+CC  sites: 598
+CC  bg_markov_order: 1
+CC  consensus.strict: gaaaaACAAaagaaTGga
+CC  consensus.strict.rc: TCCATTCTTTTGTTTTTC
+CC  consensus.IUPAC: rmrrrASAAwrsamTGsa
+CC  consensus.IUPAC.rc: TSCAKTSYWTTSTYYYKY
+CC  consensus.regexp: [ag][ac][ag][ag][ag]A[CG]AA[at][ag][cg]a[ac]TG[cg]a
+CC  consensus.regexp.rc: T[CG]CA[GT]T[CG][CT][AT]TT[CG]T[CT][CT][CT][GT][CT]
+XX
+//
+AC  oligos_6nt_vs_ctrl_m3
+XX
+ID  oligos_6nt_vs_ctrl_m3
+XX
+DE  aaAAAAAAAAAARRAaa
+PO       A     C     G     T
+1     1310   284   325   370
+2     1406   315   281   287
+3     1799   486     4     0
+4     2286     0     3     0
+5     2287     1     0     1
+6     1864     0   425     0
+7     1672   146   385    86
+8     2051     0   238     0
+9     1627   447   215     0
+10    2289     0     0     0
+11    2289     0     0     0
+12    1952     0     0   337
+13    1687     0   602     0
+14    1642     1   646     0
+15    1754     5   529     1
+16    1400   286   391   212
+17    1304   335   380   270
+XX
+BA  2289 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 3
+CC  accession: oligos_6nt_vs_ctrl_m3
+CC  AC: oligos_6nt_vs_ctrl_m3
+CC  id: oligos_6nt_vs_ctrl_m3
+CC  version: 
+CC  name: oligos_6nt_vs_ctrl_m3
+CC  description: aaAAAAAAAAAARRAaa
+CC  statistical_basis: 2289 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 3
+CC  sites: 2289
+CC  bg_markov_order: 1
+CC  consensus.strict: aaAAAAAAAAAAAAAaa
+CC  consensus.strict.rc: TTTTTTTTTTTTTTTTT
+CC  consensus.IUPAC: aaAAAAAAAAAARRAaa
+CC  consensus.IUPAC.rc: TTTYYTTTTTTTTTTTT
+CC  consensus.regexp: aaAAAAAAAAAA[AG][AG]Aaa
+CC  consensus.regexp.rc: TTT[CT][CT]TTTTTTTTTTTT
+XX
+//
+AC  oligos_7nt_vs_ctrl_m1
+XX
+ID  oligos_7nt_vs_ctrl_m1
+XX
+DE  aamaraACAAwrgvaa
+PO       A     C     G     T
+1     1217   658   820   763
+2     1187   825   833   613
+3     1719  1734     2     3
+4     2285     1   618   554
+5     1338   435  1685     0
+6     2197   441   820     0
+7     3458     0     0     0
+8        0  3458     0     0
+9     3458     0     0     0
+10    3458     0     0     0
+11    2021     0     0  1437
+12    1569     0  1889     0
+13     811   602  2045     0
+14    1591   903   964     0
+15    1248   820   771   619
+16    1146   847   722   743
+XX
+BA  3458 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 4
+CC  accession: oligos_7nt_vs_ctrl_m1
+CC  AC: oligos_7nt_vs_ctrl_m1
+CC  id: oligos_7nt_vs_ctrl_m1
+CC  version: 
+CC  name: oligos_7nt_vs_ctrl_m1
+CC  description: aamaraACAAwrgvaa
+CC  statistical_basis: 3458 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 4
+CC  sites: 3458
+CC  bg_markov_order: 1
+CC  consensus.strict: aacagaACAAaggaaa
+CC  consensus.strict.rc: TTTCCTTTGTTCTGTT
+CC  consensus.IUPAC: armaraACAAwrgvam
+CC  consensus.IUPAC.rc: KTBCYWTTGTTYTKYT
+CC  consensus.regexp: a[ag][ac]a[ag]aACAA[at][ag]g[acg]a[ac]
+CC  consensus.regexp.rc: [GT]T[CGT]C[CT][AT]TTGTT[CT]T[GT][CT]T
+XX
+//
+AC  oligos_7nt_vs_ctrl_m2
+XX
+ID  oligos_7nt_vs_ctrl_m2
+XX
+DE  armArAACAAwrGAmAmr
+PO       A     C     G     T
+1      174    90   125   126
+2      157   106   145   107
+3      220   248    23    24
+4      367     3    96    49
+5      171    50   280    14
+6      410     3    93     9
+7      515     0     0     0
+8        0   511     1     3
+9      496    18     0     1
+10     514     0     0     1
+11     294     1     0   220
+12     256     3   255     1
+13       4     2   505     4
+14     440    25    33    17
+15     292   183    27    13
+16     368    59    36    52
+17     146   153   104   112
+18     169    96   137   113
+XX
+BA  515 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 5
+CC  accession: oligos_7nt_vs_ctrl_m2
+CC  AC: oligos_7nt_vs_ctrl_m2
+CC  id: oligos_7nt_vs_ctrl_m2
+CC  version: 
+CC  name: oligos_7nt_vs_ctrl_m2
+CC  description: armArAACAAwrGAmAmr
+CC  statistical_basis: 515 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 5
+CC  sites: 515
+CC  bg_markov_order: 1
+CC  consensus.strict: agcAgAACAAagGAaAca
+CC  consensus.strict.rc: TGTTTCCTTTGTTCTGCT
+CC  consensus.IUPAC: rrmArAACAAwrGAmAmr
+CC  consensus.IUPAC.rc: YKTKTCYWTTGTTYTKYY
+CC  consensus.regexp: [ag][ag][ac]A[ag]AACAA[at][ag]GA[ac]A[ac][ag]
+CC  consensus.regexp.rc: [CT][GT]T[GT]TC[CT][AT]TTGTT[CT]T[GT][CT][CT]
+XX
+//
+AC  oligos_7nt_vs_ctrl_m3
+XX
+ID  oligos_7nt_vs_ctrl_m3
+XX
+DE  aamArAAMAAArrMAaa
+PO       A     C     G     T
+1      978   358   400   448
+2      990   466   380   348
+3     1308   868     4     4
+4     1612     0   327   245
+5     1262   225   696     1
+6     1539   232   413     0
+7     2184     0     0     0
+8      596  1588     0     0
+9     2184     0     0     0
+10    2184     0     0     0
+11    1640     0     0   544
+12    1142   186   856     0
+13     965   489   730     0
+14    1500   680     1     3
+15    2131    16    25    12
+16    1088   375   444   277
+17     977   506   372   329
+XX
+BA  2184 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 6
+CC  accession: oligos_7nt_vs_ctrl_m3
+CC  AC: oligos_7nt_vs_ctrl_m3
+CC  id: oligos_7nt_vs_ctrl_m3
+CC  version: 
+CC  name: oligos_7nt_vs_ctrl_m3
+CC  description: aamArAAMAAArrMAaa
+CC  statistical_basis: 2184 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 6
+CC  sites: 2184
+CC  bg_markov_order: 1
+CC  consensus.strict: aaaAaaACAAAaaaAaa
+CC  consensus.strict.rc: TTTTTTTTTGTTTTTTT
+CC  consensus.IUPAC: aamAraAMAAArrmAaa
+CC  consensus.IUPAC.rc: TTTKYYTTTKTTYTKTT
+CC  consensus.regexp: aa[ac]A[ag]aA[AC]AAA[ag][ag][ac]Aaa
+CC  consensus.regexp.rc: TTT[GT][CT][CT]TTT[GT]TT[CT]T[GT]TT
+XX
+//
+AC  dyads_vs_ctrl_m1
+XX
+ID  dyads_vs_ctrl_m1
+XX
+DE  wwArAACAAwrkwww
+PO       A     C     G     T
+1      475   207   323   339
+2      337   323   251   433
+3     1344     0     0     0
+4      349   141   854     0
+5      983    71   290     0
+6     1084    69   191     0
+7      289   948   107     0
+8     1050   294     0     0
+9     1344     0     0     0
+10     412   143     0   789
+11     493     0   634   217
+12     296     0   682   366
+13     867     0     0   477
+14     387   322   281   354
+15     407   335   240   362
+XX
+BA  1344 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 7
+CC  accession: dyads_vs_ctrl_m1
+CC  AC: dyads_vs_ctrl_m1
+CC  id: dyads_vs_ctrl_m1
+CC  version: 
+CC  name: dyads_vs_ctrl_m1
+CC  description: wwArAACAAwrkwww
+CC  statistical_basis: 1344 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 7
+CC  sites: 1344
+CC  bg_markov_order: 1
+CC  consensus.strict: atAgAACAAtggaaa
+CC  consensus.strict.rc: TTTCCATTGTTCTAT
+CC  consensus.IUPAC: rtAgAACAAwrkwwh
+CC  consensus.IUPAC.rc: DWWMYWTTGTTCTAY
+CC  consensus.regexp: [ag]tAgAACAA[at][ag][gt][at][at][act]
+CC  consensus.regexp.rc: [AGT][AT][AT][AC][CT][AT]TTGTTCTA[CT]
+XX
+//
+AC  dyads_vs_ctrl_m2
+XX
+ID  dyads_vs_ctrl_m2
+XX
+DE  amArRrmmWwrdwATdw
+PO       A     C     G     T
+1      629   349   385   396
+2      578   451   361   369
+3     1759     0     0     0
+4      743   297   719     0
+5     1260     0   499     0
+6      829   374   556     0
+7      763   653   343     0
+8     1185   574     0     0
+9     1216     0     0   543
+10     736   193   300   530
+11     745     0   685   329
+12     648     0   626   485
+13     900     0   404   455
+14    1759     0     0     0
+15       0     0     0  1759
+16     492   342   473   452
+17     531   419   347   462
+XX
+BA  1759 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 8
+CC  accession: dyads_vs_ctrl_m2
+CC  AC: dyads_vs_ctrl_m2
+CC  id: dyads_vs_ctrl_m2
+CC  version: 
+CC  name: dyads_vs_ctrl_m2
+CC  description: amArRrmmWwrdwATdw
+CC  statistical_basis: 1759 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 8
+CC  sites: 1759
+CC  bg_markov_order: 1
+CC  consensus.strict: aaAgAaaaaaggaATga
+CC  consensus.strict.rc: TCATTCCTTTTTTCTTT
+CC  consensus.IUPAC: amArRrmmwwrdaATrw
+CC  consensus.IUPAC.rc: WYATTHYWWKKYYYTKT
+CC  consensus.regexp: a[ac]A[ag][AG][ag][ac][ac][at][at][ag][agt]aAT[ag][at]
+CC  consensus.regexp.rc: [AT][CT]ATT[ACT][CT][AT][AT][GT][GT][CT][CT][CT]T[GT]T
+XX
+//
+AC  dyads_vs_ctrl_m3
+XX
+ID  dyads_vs_ctrl_m3
+XX
+DE  raArACAawrGAaa
+PO       A     C     G     T
+1      662   433   549   489
+2      688   504   501   440
+3     1699   434     0     0
+4     1055   470   608     0
+5     1720   413     0     0
+6        0  2133     0     0
+7     1748     0     0   385
+8     1427     0   371   335
+9     1063     0   358   712
+10     995     0  1138     0
+11       0     0  2133     0
+12    2133     0     0     0
+13     746   454   533   400
+14     699   482   425   527
+XX
+BA  2133 sequences
+XX
+CC  program: transfac
+CC  matrix.nb: 9
+CC  accession: dyads_vs_ctrl_m3
+CC  AC: dyads_vs_ctrl_m3
+CC  id: dyads_vs_ctrl_m3
+CC  version: 
+CC  name: dyads_vs_ctrl_m3
+CC  description: raArACAawrGAaa
+CC  statistical_basis: 2133 sequences
+CC  transfac_consensus: 
+CC  matrix.nb: 9
+CC  sites: 2133
+CC  bg_markov_order: 1
+CC  consensus.strict: aaAaACAaagGAaa
+CC  consensus.strict.rc: TTTCCTTTGTTTTT
+CC  consensus.IUPAC: raArACAawrGAra
+CC  consensus.IUPAC.rc: TYTCYWTTGTYTTY
+CC  consensus.regexp: [ag]aA[ag]ACAa[at][ag]GA[ag]a
+CC  consensus.regexp.rc: T[CT]TC[CT][AT]TTGT[CT]TT[CT]
+XX
+//
