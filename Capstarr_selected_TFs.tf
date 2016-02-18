ffc3ed1e9bfb7da6dad7d0907756fd9a64697902
Regulatory Sequence Analysis Tools account <Jacques.van-Helden@univ-amu.fr>
2016-02-18 01:19:56 +0100

    New demo files for motif enrichment

---
+++ Capstarr_selected_TFs.tf
@@ -0,0 +1,324 @@
+AC  JUN_FOS
+XX
+ID  JUN_FOS
+XX
+DE  sATGAsTCAyyc
+P0       A     C     G     T
+1    19885 28200 36669 12428
+2    90233 16150 22086  1891
+3      239    77   228 126719
+4      101   219 123170  3778
+5    126551   292   117   305
+6     6751 41768 77395  1398
+7      202   294   193 126584
+8     2753 124383    88    33
+9    127011    56    67   118
+10    1670 47868 18264 60915
+11   29868 45298 11779 36425
+12    5816  8844  4880  4770
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: merge_21
+CC  AC: merge_21
+CC  id: merge_21
+CC  name: merge_21
+CC  version: 
+CC  name: merge_21
+CC  description: sATGAsTCAyyc
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: gATGAgTCAtcc
+CC  consensus.strict.rc: GGATGACTCATC
+CC  consensus.IUPAC: sATGAsTCAyyc
+CC  consensus.IUPAC.rc: GRRTGASTCATS
+CC  consensus.regexp: [cg]ATGA[cg]TCA[ct][ct]c
+CC  consensus.regexp.rc: G[AG][AG]TGA[CG]TCAT[CG]
+CC  merged_AC: Hocomoco_M01506,Hocomoco_M01141,Hocomoco_M01282,Hocomoco_M01142,Jaspar_MA0478.1,Jaspar_MA0490.1,Jaspar_MA0841.1,Hocomoco_M01139,Hocomoco_M01140,Hocomoco_M01283,Jaspar_MA0477.1,Jaspar_MA0476.1,Jaspar_MA0491.1,Hocomoco_M01031,Hocomoco_M01284,Jaspar_MA0655.1
+CC  merge_nb: 16
+CC  merged_ID: SMRC1,FOSL2,JUNB,FOS,FOSL2,JUNB,NFE2,FOSB,FOSL1,JUND,FOSL1,FOS,JUND,BATF,JUN,JDP2
+CC  type: merge
+CC  merge_type: sum
+XX
+//
+AC  YY
+XX
+ID  YY
+XX
+DE  cAArATGGCggcc
+P0       A     C     G     T
+1     1481  5456  1030   804
+2     8927    83   358   214
+3     8772   348   404   763
+4     3778  1543  4270   551
+5    10107    13     8    22
+6       34    21     8 10117
+7       18     0 10128     0
+8       59     2 10051    42
+9     1044  8197   103   870
+10    1068  2027  5025  2065
+11    1342  1145  6879  1050
+12    1990  5970   751  1430
+13      93   239    90   137
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: merge_47
+CC  AC: merge_47
+CC  id: merge_47
+CC  name: merge_47
+CC  version: 
+CC  name: merge_47
+CC  description: cAArATGGCggcc
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: cAAgATGGCggcc
+CC  consensus.strict.rc: GGCCGCCATCTTG
+CC  consensus.IUPAC: cAArATGGCggcc
+CC  consensus.IUPAC.rc: GGCCGCCATYTTG
+CC  consensus.regexp: cAA[ag]ATGGCggcc
+CC  consensus.regexp.rc: GGCCGCCAT[CT]TTG
+CC  merge_nb: 4
+CC  type: merge
+CC  merge_type: sum
+CC  merged_AC: Hocomoco_M01588,Jaspar_MA0095.2,Hocomoco_M01589,Jaspar_MA0748.1
+CC  merged_ID: TYY1,YY1,TYY2,YY2
+XX
+//
+AC  REST
+XX
+ID  REST
+XX
+DE  tTCAGcACCayGGACAGckcCm
+P0       A     C     G     T
+1      272   240   474  1104
+2       67   368   180  1481
+3       92  1801    69   134
+4     1895    50   116    40
+5       60    59  1972    12
+6      166  1303   408   227
+7     2036    20    32    17
+8       14  2046    34    11
+9       45  1927    39    94
+10    1196   252   238   418
+11     293   527   173  1110
+12      49    12  2031    11
+13      29    16  2054     4
+14    1846   149    48    59
+15      17  1689   299    94
+16    2049    17    16    17
+17      50    19  2008    16
+18     250  1367   228   247
+19     456    50   907   678
+20     258  1267   412   152
+21     234  1481    53   321
+22     137   221    61    80
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: merge_100
+CC  AC: merge_100
+CC  id: merge_100
+CC  name: merge_100
+CC  version: 
+CC  name: merge_100
+CC  description: tTCAGcACCayGGACAGckcCm
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: tTCAGcACCatGGACAGcgcCc
+CC  consensus.strict.rc: GGGCGCTGTCCATGGTGCTGAA
+CC  consensus.IUPAC: tTCAGcACCayGGACAGckcCm
+CC  consensus.IUPAC.rc: KGGMGCTGTCCRTGGTGCTGAA
+CC  consensus.regexp: tTCAGcACCa[ct]GGACAGc[gt]cC[ac]
+CC  consensus.regexp.rc: [GT]GG[AC]GCTGTCC[AG]TGGTGCTGAA
+CC  type: merge
+CC  merged_ID: REST,REST
+CC  merge_nb: 2
+CC  merged_AC: Hocomoco_M01481,Jaspar_MA0138.2
+CC  merge_type: sum
+XX
+//
+AC  SP
+XX
+ID  SP
+XX
+DE  ggggrgGGGGcGGGGcsrgsgs
+P0       A     C     G     T
+1      596   679  1843   314
+2      821   728  1815   483
+3      630   644  2191   382
+4      934   837  1696   380
+5     1309   724  1287   527
+6      784   503  2267   293
+7      436   123  2758   530
+8      682    42  3094    29
+9       29    20  3763    35
+10      29    52  3756    10
+11     655  2605    89   498
+12      85    51  3576   135
+13      53    60  3553   181
+14     611   205  2874   157
+15     399   351  2936   161
+16     531  2142   769   405
+17     351  1674  1016   806
+18     997   611  1472   767
+19     755   848  1869   375
+20     159   239   421    95
+21     151   193   481    89
+22      61   163   247    28
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: merge_149
+CC  AC: merge_149
+CC  id: merge_149
+CC  name: merge_149
+CC  version: 
+CC  name: merge_149
+CC  description: ggggrgGGGGcGGGGcsrgsgs
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: ggggagGGGGcGGGGccggggg
+CC  consensus.strict.rc: CCCCCGGCCCCGCCCCCTCCCC
+CC  consensus.IUPAC: ggggrgGGGGcGGGGcsrgsgs
+CC  consensus.IUPAC.rc: SCSCYSGCCCCGCCCCCYCCCC
+CC  consensus.regexp: gggg[ag]gGGGGcGGGGc[cg][ag]g[cg]g[cg]
+CC  consensus.regexp.rc: [CG]C[CG]C[CT][CG]GCCCCGCCCCC[CT]CCCC
+CC  merged_ID: SP1,SP2,SP3
+CC  type: merge
+CC  merge_nb: 3
+CC  merged_AC: Hocomoco_M01524,Hocomoco_M01526,Hocomoco_M01527
+CC  merge_type: sum
+XX
+//
+AC  DDIT
+XX
+ID  DDIT
+XX
+DE  gggkATTGCAyyy
+P0       A     C     G     T
+1        9     4    27     1
+2       16    13    51     0
+3       11    16    46     7
+4        5     8    42    25
+5       67     0    11     2
+6        0     2     3    75
+7        0     0     1    79
+8        3     0    70     7
+9        1    74     5     0
+10      75     0     5     0
+11       9    26    11    34
+12      12    24    16    28
+13      11    25    18    26
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: merge_125
+CC  AC: merge_125
+CC  id: merge_125
+CC  name: merge_125
+CC  version: 
+CC  name: merge_125
+CC  description: gggkATTGCAyyy
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: ggggATTGCAttt
+CC  consensus.strict.rc: AAATGCAATCCCC
+CC  consensus.IUPAC: gggkATTGCAyyy
+CC  consensus.IUPAC.rc: RRRTGCAATMCCC
+CC  consensus.regexp: ggg[gt]ATTGCA[ct][ct][ct]
+CC  consensus.regexp.rc: [AG][AG][AG]TGCAAT[AC]CCC
+CC  merged_AC: Hocomoco_M01075,Jaspar_MA0019.1
+CC  merge_nb: 2
+CC  merge_type: sum
+CC  type: merge
+CC  merged_ID: DDIT3,Ddit3::Cebpa
+XX
+//
+AC  FCP2_GRHL1
+XX
+ID  FCP2_GRHL1
+XX
+DE  aAAACCGGTTtd
+P0       A     C     G     T
+1     1345   472   247   427
+2     2830   170   538   465
+3     3066     9   136    94
+4     3066     3    15   130
+5        1  3066     2     0
+6      376  3066    52   340
+7      461    55  3066   207
+8        1    10  3066     1
+9      265    39     2  3066
+10     173   246    32  3066
+11     857   733   275  2743
+12     685   289   655   862
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: merge_190
+CC  AC: merge_190
+CC  id: merge_190
+CC  name: merge_190
+CC  version: 
+CC  name: merge_190
+CC  description: aAAACCGGTTtd
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  consensus.strict: aAAACCGGTTtt
+CC  consensus.strict.rc: AAAACCGGTTTT
+CC  consensus.IUPAC: aAAACCGGTTtd
+CC  consensus.IUPAC.rc: HAAACCGGTTTT
+CC  consensus.regexp: aAAACCGGTTt[agt]
+CC  consensus.regexp.rc: [ACT]AAACCGGTTTT
+CC  merge_nb: 2
+CC  merge_type: sum
+CC  merged_ID: TFCP2,GRHL1
+CC  type: merge
+CC  merged_AC: Jaspar_MA0145.3,Jaspar_MA0647.1
+XX
+//
+AC  USF2
+XX
+ID  USF2
+XX
+DE  ggGTCACGTGrssvsssss
+P0       A     C     G     T
+1      108   104   224    64
+2      102    70   273    55
+3       58     8   432     2
+4        8    24    18   450
+5        1   499     0     0
+6      496     0     2     2
+7        0   463    11    26
+8       51     0   449     0
+9        2     4     2   492
+10       1     0   498     1
+11     169    84   198    49
+12      30   224   133   113
+13      60   232   167    41
+14     126   149   165    60
+15      64   185   183    68
+16      63   165   192    80
+17      81   163   194    62
+18      64   189   157    90
+19      61   137   193   109
+XX
+CC  program: transfac
+CC  matrix.nb: 1
+CC  accession: merge_361
+CC  AC: Hocomoco_M01593
+CC  id: Hocomoco_M01593
+CC  name: merge_361
+CC  version: 
+CC  name: merge_361
+CC  description: ggGTCACGTGrssvsssss
+CC  transfac_consensus: 
+CC  matrix.nb: 1
+CC  merged_AC: Hocomoco_M01593
+CC  merged_ID: USF2
+CC  merge_type: sum
+CC  type: merge
+CC  merge_nb: 1
+XX
+//
